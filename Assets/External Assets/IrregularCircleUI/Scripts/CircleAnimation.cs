ff75dc0f1b9b716eca41408368fa0d50c373e0b3
20shaked20 <shakedlevi32@gmail.com>
2024-01-20 22:48:42 +0200

    die lint

--- Assets/External Assets/IrregularCircleUI/Scripts/CircleAnimation.cs
+++ Assets/External Assets/IrregularCircleUI/Scripts/CircleAnimation.cs
@@ -1,15 +1,17 @@
 ﻿using UnityEngine;
+
 public class CircleAnimation : MonoBehaviour
 {
-	[SerializeField] GameObject[] animObjects;
-	private float angle_curve = 50f;
-	void Update()
-	{
-		foreach (GameObject go in animObjects)
-		{
-			Vector3 angle = go.transform.eulerAngles;
-			angle.z += Time.deltaTime * angle_curve;
-			go.transform.eulerAngles = angle;
-		}
-	}
+    [SerializeField] GameObject[] animObjects;
+    private float angle_curve = 50f;
+
+    void Update()
+    {
+        foreach (GameObject go in animObjects)
+        {
+            Vector3 angle = go.transform.eulerAngles;
+            angle.z += Time.deltaTime * angle_curve;
+            go.transform.eulerAngles = angle;
+        }
+    }
 }
