6156a957b63e01ba0b45c5df1e0d9b19c86c4932
20shaked20 <shakedlevi32@gmail.com>
2024-01-20 13:30:10 +0200

    lint fix

--- Assets/Scripts/2-spawners/TimedSpawnerRandom.cs
+++ Assets/Scripts/2-spawners/TimedSpawnerRandom.cs
@@ -5,28 +5,33 @@ using UnityEngine;
 /**
  * This component instantiates a given prefab at random time intervals and random bias from its object position.
  */
-public class TimedSpawnerRandom: MonoBehaviour {
+public class TimedSpawnerRandom : MonoBehaviour
+{
     int PrefabTypes = 5;
+    int zero = 0;
     [SerializeField] GameObject[] prefabToSpawn;
     [SerializeField] Vector3 velocityOfSpawnedObject;
-    [Tooltip("Minimum time between consecutive spawns, in seconds")] [SerializeField] float minTimeBetweenSpawns = 0.2f;
-    [Tooltip("Maximum time between consecutive spawns, in seconds")] [SerializeField] float maxTimeBetweenSpawns = 1.0f;
-    [Tooltip("Maximum distance in X between spawner and spawned objects, in meters")] [SerializeField] float maxXDistance = 1.5f;
+    [Tooltip("Minimum time between consecutive spawns, in seconds")][SerializeField] float minTimeBetweenSpawns = 0.2f;
+    [Tooltip("Maximum time between consecutive spawns, in seconds")][SerializeField] float maxTimeBetweenSpawns = 1.0f;
+    [Tooltip("Maximum distance in X between spawner and spawned objects, in meters")][SerializeField] float maxXDistance = 1.5f;
 
-    void Start() {
-         this.StartCoroutine(SpawnRoutine());    // co-routines
+    void Start()
+    {
+        this.StartCoroutine(SpawnRoutine());    // co-routines
     }
 
-    IEnumerator SpawnRoutine() {    // co-routines
-        while (true) {
+    IEnumerator SpawnRoutine()
+    {    // co-routines
+        while (true)
+        {
             float timeBetweenSpawnsInSeconds = Random.Range(minTimeBetweenSpawns, maxTimeBetweenSpawns);
             yield return new WaitForSeconds(timeBetweenSpawnsInSeconds);       // co-routines
             Vector3 positionOfSpawnedObject = new Vector3(
                 transform.position.x + Random.Range(-maxXDistance, +maxXDistance),
                 transform.position.y,
                 transform.position.z);
-            
-            GameObject newObject = Instantiate(prefabToSpawn[Random.Range(0,PrefabTypes)], positionOfSpawnedObject, Quaternion.identity);
+
+            GameObject newObject = Instantiate(prefabToSpawn[Random.Range(zero, PrefabTypes)], positionOfSpawnedObject, Quaternion.identity);
             newObject.GetComponent<Mover>().SetVelocity(velocityOfSpawnedObject);
         }
     }
--- Assets/Scripts/3-collisions/ScoreAdder.cs
+++ Assets/Scripts/3-collisions/ScoreAdder.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 /**
  * This component increases a given "score" field whenever it is triggered.
  */
-public class ScoreAdder : MonoBehaviour {
+public class ScoreAdder : MonoBehaviour
+{
     [Tooltip("Every object tagged with this tag will trigger adding score to the score field.")]
     [SerializeField] string triggeringTag;
     [SerializeField] NumberField scoreField;
@@ -12,18 +13,19 @@ public class ScoreAdder : MonoBehaviour {
     [SerializeField] int BossPoints;
     [SerializeField] LayerMask BossLayer;
     [SerializeField] LayerMask NormalEnemyLayer;
-    private void OnTriggerEnter2D(Collider2D other) {
-        if (other.tag == triggeringTag && scoreField!=null) {
-            Debug.Log(other.gameObject.layer);
-            Debug.Log(BossLayer.value);
-            if(BossLayer == ( BossLayer | ( 1 << other.gameObject.layer))) // had to shift left because of how unity works..
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == triggeringTag && scoreField != null)
+        {
+            if (BossLayer == (BossLayer | (1 << other.gameObject.layer))) // had to shift left because of how unity works..
                 scoreField.AddNumber(BossPoints);
-            if(NormalEnemyLayer == ( NormalEnemyLayer | ( 1 << other.gameObject.layer))) // had to shift left because of how unity works..
+            if (NormalEnemyLayer == (NormalEnemyLayer | (1 << other.gameObject.layer))) // had to shift left because of how unity works..
                 scoreField.AddNumber(NormalEnemyPoints);
         }
     }
 
-    public void SetScoreField(NumberField newTextField) {
+    public void SetScoreField(NumberField newTextField)
+    {
         this.scoreField = newTextField;
     }
 }
