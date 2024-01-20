dfbc4e6fae6d5fb4a1aa770b0fc998edbd5af41a
20shaked20 <shakedlevi32@gmail.com>
2024-01-20 22:41:01 +0200

    lint forever

--- Assets/External Assets/IrregularCircleUI/Scripts/CircleAnimation.cs
+++ Assets/External Assets/IrregularCircleUI/Scripts/CircleAnimation.cs
@@ -1,17 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UIElements;
 public class CircleAnimation : MonoBehaviour
 {
-	public GameObject[] animObjects;
+	[SerializeField] GameObject[] animObjects;
 	// Update is called once per frame
+	private float angle_curve = 50f;
 	void Update()
 	{
 		foreach (GameObject go in animObjects)
 		{
 			Vector3 angle = go.transform.eulerAngles;
-			angle.z += Time.deltaTime * 50f;
+			angle.z += Time.deltaTime * angle_curve;
 			go.transform.eulerAngles = angle;
 		}
 	}
