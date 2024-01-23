40d3d3411c3a8eb5965cea9d5b260f3b80262544
20shaked20 <shakedlevi32@gmail.com>
2024-01-23 12:09:31 +0200

    lint

--- Assets/Scripts/3-collisions/DestroyOnTrigger2D.cs
+++ Assets/Scripts/3-collisions/DestroyOnTrigger2D.cs
@@ -1,18 +1,15 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 /**
  * This component destroys its object whenever it triggers a 2D collider with the given tag.
  */
 public class DestroyOnTrigger2D : MonoBehaviour
 {
     [SerializeField] string[] triggeringTag;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         for(int i = 0; i<triggeringTag.Length; ++ i)
             if (other.tag == triggeringTag[i] && enabled)
-            {
                 Destroy(this.gameObject);
-            }
     }
 }
